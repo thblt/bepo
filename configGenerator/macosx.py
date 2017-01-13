6133dcc88a99246ff61f5ef263a1f1a163e9b5be
Gaétan RYCKEBOER <gryckeboer@jouve.com>
2017-01-13 17:14:40 +0100

    Indentation du code (linter compliant)

--- configGenerator/macosx.py
+++ configGenerator/macosx.py
@@ -11,13 +11,16 @@
 # of the License, or (at your option) any later version.
 #
 
-import defaults, sys
+import defaults
+import sys
+import xkb
+import dead_keys
+import codecs
 defaults.xkbFile = sys.argv[1]
 
-import xkb, dead_keys, codecs
 
 xkb.tmplValues[u"actionsAndTerminators"] = dead_keys.deadXMLCode
-for k,v in xkb.tmplValues.iteritems():
-  xkb.tmplValues[k] = dead_keys.xmlChar(v)
+for k, v in xkb.tmplValues.iteritems():
+    xkb.tmplValues[k] = dead_keys.xmlChar(v)
 out = codecs.open(sys.argv[2], "w", "utf8")
-out.write( xkb.tmpl % xkb.tmplValues )
+out.write(xkb.tmpl % xkb.tmplValues)
